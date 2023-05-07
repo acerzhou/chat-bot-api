3734bd7ed92c8331c0dfb4d0e5bc6be90a7f5568
Acer Zhou <acerzhou.work@gmail.com>
2023-05-07 15:57:13 +1000

    Update post cors

--- user-info/app.mjs
+++ user-info/app.mjs
@@ -26,6 +26,11 @@ export const lambdaHandler = async (event, context) => {
 
     const response = {
       statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
+
       body: JSON.stringify({ message: "POST request processed successfully" }),
     };
 
