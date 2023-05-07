c8593f461ff66db6be277e952a70255f3f289a6b
Acer Zhou <acerzhou.work@gmail.com>
2023-05-07 10:48:37 +1000

    Enable cors

--- user-info/app.mjs
+++ user-info/app.mjs
@@ -22,6 +22,10 @@ export const lambdaHandler = async (event, context) => {
 
     return {
       statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
       body: JSON.stringify(body),
     };
   } catch (err) {
