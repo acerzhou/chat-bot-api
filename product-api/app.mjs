218e4a7406b6597854c821f478549cdc14368b0c
Acer Zhou <acerzhou.work@gmail.com>
2023-05-11 21:18:57 +1000

    Add more info

--- product-api/app.mjs
+++ product-api/app.mjs
@@ -16,27 +16,32 @@ export const lambdaHandler = async (event, context) => {
     const products = [
       {
         id: "1",
-        sku: "",
-        name: "",
-        description: "",
-        brand: "",
-        price: "",
+        sku: "609877",
+        name: "The Legend of Zelda:Tears of the Kingdom",
+        description: "The most popular game",
+        brand: "Nintedo Switch",
+        price: 74.0,
+        image:
+          "https://www.jbhifi.com.au/products/nintendo-switch-the-legend-of-zeldatears-of-the-kingdom?queryID=9332e3671fb1fba6cb64cc68cc30f11a&objectID=609877",
       },
       {
         id: "2",
-        sku: "",
-        name: "",
-        description: "",
-        brand: "",
-        price: "",
+        sku: "294330",
+        name: "Born To Die (Vinyl)",
+        description: "A good vinyl",
+        brand: "lana del rey",
+        price: 61.99,
+        image:
+          "https://www.jbhifi.com.au/products/vinyl-del-rey-lana-born-to-die-vinyl-2lp?queryID=18eeed8ec5067cabf934385eb8111f8c&objectID=294330",
       },
       {
-        id: "1",
-        sku: "",
-        name: "",
-        description: "",
-        brand: "",
-        price: "",
+        id: "3",
+        sku: "348343",
+        name: " Apple Pencil (2nd Gen)",
+        description: "Apple Pen",
+        brand: "Apple",
+        price: 158,
+        image: "https://www.jbhifi.com.au/products/apple-pencil-2nd-gen",
       },
     ];
 
