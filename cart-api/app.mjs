b3fd3010f8530c003ba5f0bd922b4949fc4a2eb4
Acer Zhou <acerzhou.work@gmail.com>
2023-05-15 21:26:05 +1000

    Update chat api

--- cart-api/app.mjs
+++ cart-api/app.mjs
@@ -1,4 +1,8 @@
-import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
+import {
+  DynamoDBClient,
+  PutItemCommand,
+  GetItemCommand,
+} from "@aws-sdk/client-dynamodb";
 
 export const lambdaHandler = async (event, context) => {
   const { httpMethod } = event;
@@ -34,47 +38,44 @@ const handleCartUpdate = async (event) => {
   try {
     const results = await dbClient.send(command);
     console.log(results);
-  } catch (err) {
-    console.error(err);
-  }
-};
-
-const handleGetCart = () => {
-  try {
-    const cart = {
-      id: 1,
-      type: "cart",
-      items: [
-        {
-          id: 1,
-          name: "The Legend of Zelda:Tears of the Kingdom",
-          sku: "609877",
-          price: 74.0,
-          image: "/images/zelda.webp",
-          quantity: 1,
-        },
-        {
-          id: 2,
-          name: "Born To Die (Vinyl)",
-          sku: "294330",
-          price: 61.99,
-          image: "/images/born-to-die.webp",
-          quantity: 2,
-        },
-      ],
-      totalPrice: 0,
-    };
-
     return {
       statusCode: 200,
       headers: {
         "Access-Control-Allow-Origin": "*",
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(cart),
+      body: "Cart updated",
     };
   } catch (err) {
-    console.log(err);
-    return err;
+    console.error(err);
+  }
+};
+
+const handleGetCart = async () => {
+  const params = {
+    TableName: "CartTable",
+    Key: {
+      id: 1,
+    },
+  };
+
+  const dbClient = new DynamoDBClient({
+    region: "ap-southeast-2",
+  });
+  const command = new GetItemCommand(params);
+
+  try {
+    const results = await dbClient.send(command);
+    console.log(results);
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
+      body: "Cart updated",
+    };
+  } catch (err) {
+    console.error(err);
   }
 };
